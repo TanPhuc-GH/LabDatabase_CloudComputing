de9d0ec0c4ccd524e84a2c6096f232fe0ac422c2
TanPhuc-GH <truongtanphuc2002@gmail.com>
2022-10-31 01:06:23 +0700

    upload

--- insertPost.py
+++ insertPost.py
@@ -3,7 +3,7 @@ import psycopg2
 
 
 #Tạo kết nối với RDS PostgreSQL và thêm dữ liệu vào
-conn = psycopg2.connect(database='Postgres_covid19', user='postgres', password='12345678', host='database-postgres.cfcivdrjixtu.us-east-1.rds.amazonaws.com', port='5432')
+conn = psycopg2.connect(database='Postgres_covid19', user='postgres', password='12345678', host='database-postgres.csrogzximx9k.us-east-1.rds.amazonaws.com', port='5432')
 print('Open DB successfully')
 ut.insert(conn)
 ut.select(conn)
